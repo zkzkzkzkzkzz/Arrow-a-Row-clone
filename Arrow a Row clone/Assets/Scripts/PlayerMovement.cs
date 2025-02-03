33bd9c0de764f8cbffafff2126bfd709b982b205
zkzkzkzkzkzz <sto134679@gmail.com>
2025-02-03 14:02:24 +0900

    플레이어 좌우 이동 적용
    Rigidbody 모델에 추가하여 스크립트 적용

--- Arrow a Row clone/Assets/Scripts/PlayerMovement.cs
+++ Arrow a Row clone/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 3f;
+    private Rigidbody rb;
+    private float moveInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        if (rb != null )
+        {
+            Debug.LogError("Rigidbody를 찾을 수 없습니다.");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // A, D 또는 방향키로 좌우 이동
+        moveInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(moveInput * moveSpeed, rb.velocity.y, 0);
     }
 }
