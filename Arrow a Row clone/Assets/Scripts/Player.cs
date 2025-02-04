eb2c735cf443c8f841240e1ca6477ec3bde85fa6
zkzkzkzkzkzz <sto134679@gmail.com>
2025-02-04 09:37:00 +0900

    FEAT / 플레이어 스탯 추가

--- Arrow a Row clone/Assets/Scripts/Player.cs
+++ Arrow a Row clone/Assets/Scripts/Player.cs
@@ -10,12 +10,58 @@ public class Player : MonoBehaviour
     private bool isWalking;
     private bool isLeft;
 
+    [System.Serializable]
+    public struct PlayerStats
+    {
+        // Player
+        public int     HP;
+        public float   moveSpeed;
+
+        // Arrow
+        public int ArrowATK;
+        public int ArrowRate;
+        public int ArrowSpeed;
+        public int ArrowRange;
+        public int ArrowCnt;
+         
+        // Sword
+        public float SwordATK;
+        public float SwordRate;
+        public float SwordSpeed;
+        public int SwordRange;
+        public int SwordCnt;
+    }
+
+    [SerializeField] private PlayerStats stats;
+
+    public void SetPlayerStats(int _HP, int _moveSpeed,
+                        int _ArrowATK, int _ArrowRate, int _ArrowSpeed, int _ArrowRange, int _ArrowCnt,
+                        float _SwordATK, float _SwordRate, float _SwordSpeed, int _SwordRange, int _SwordCnt)
+    {
+        stats.HP = _HP;
+        stats.moveSpeed = _moveSpeed;
+        
+        stats.ArrowATK = _ArrowATK;
+        stats.ArrowRate = _ArrowRate;
+        stats.ArrowSpeed = _ArrowSpeed;
+        stats.ArrowRange = _ArrowRange;
+        stats.ArrowCnt = _ArrowCnt;
+
+        stats.SwordATK = _SwordATK;
+        stats.SwordRate = _SwordRate;
+        stats.SwordSpeed = _SwordSpeed;
+        stats.SwordRange = _SwordRange;
+        stats.SwordCnt = _SwordCnt;
+    }
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
         if (rb == null)
             Debug.LogError("Rigidbody를 찾을 수 없습니다.");
+
+        SetPlayerStats(100, 3, 10, 10, 6, 10, 1, 8f, 12f, 4f, 10, 0);
     }
 
 
@@ -50,5 +96,11 @@ public class Player : MonoBehaviour
     public bool isLeftWalk()
     {
         return isLeft;
+    }
+
+
+    public PlayerStats GetPlayerStats()
+    {
+        return stats;
     }
 }
