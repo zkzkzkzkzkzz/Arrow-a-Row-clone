1569169149ae03aa862a3261cfb5ae5b63e6d4df
zkzkzkzkzkzz <sto134679@gmail.com>
2025-02-07 15:18:03 +0900

    FEAT / 타일 템플릿 스폰 및 재활용 구현
    플레이어를 향해 다가오는 방식 추가 구현

---
+++ Arrow a Row clone/Assets/Scripts/MapTileMgr.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTileMgr : MonoBehaviour
+{
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private Transform lastTileCheck;
+    [SerializeField] private float tileSpeed = 3f;
+    [SerializeField] private float tileLength = 25f;
+    [SerializeField] private float recycleZ = -21f;
+
+    [SerializeField] private List<GameObject> mapTileTemplates;
+
+    private List<GameObject> activeTiles = new List<GameObject>();
+    private Queue<GameObject> recyclePool = new Queue<GameObject>();
+
+    private void Start()
+    {
+        // 초기 타일 생성
+        for (int i = 0; i < 3; ++i)
+        {
+            SpawnTile();
+        }
+    }
+
+    private void Update()
+    {
+        foreach (GameObject tile in activeTiles)
+        {
+            tile.transform.Translate(0, 0, -tileSpeed * Time.deltaTime);
+        }
+
+        CheckTiles();
+
+        if (activeTiles.Count == 0 || isNeedNewTile())
+        {
+            SpawnTile();
+        }
+    }
+
+
+
+    private void SpawnTile()
+    {
+        GameObject tile = null;
+
+        if (recyclePool.Count > 0)
+        {
+            tile = recyclePool.Dequeue();
+            tile.SetActive(true);
+        }
+        else
+        {
+            int idx = Random.Range(0, mapTileTemplates.Count);
+            tile = Instantiate(mapTileTemplates[idx]);
+        }
+
+        Vector3 spawnPos = spawnPoint.position;
+        if (activeTiles.Count > 0)
+        {
+            GameObject lastTile = activeTiles[activeTiles.Count - 1];
+            spawnPos = lastTile.transform.position + new Vector3(0, 0, tileLength);
+        }
+        tile.transform.position = spawnPos;
+
+        activeTiles.Add(tile);
+    }
+
+    /// <summary>
+    /// 타일 재활용 체크 함수
+    /// 타일의 z값이 recycleZ보다 작아지면 재활용 풀에 넣음
+    /// </summary>
+    private void CheckTiles()
+    {
+        while (activeTiles.Count > 0)
+        {
+            GameObject tile = activeTiles[0];
+            if (tile.transform.position.z <= recycleZ)
+            {
+                tile.SetActive(false);
+                activeTiles.RemoveAt(0);
+                recyclePool.Enqueue(tile);
+            }
+            else
+                break;
+        }
+    }
+
+    private bool isNeedNewTile()
+    {
+        GameObject lastTile = activeTiles[activeTiles.Count - 1];
+        return lastTile.transform.position.z < lastTileCheck.position.z;
+    }
+}
