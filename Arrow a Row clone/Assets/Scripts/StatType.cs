1d37948fee9858be5331b6a7d52f9ec5ecf308b3
zkzkzkzkzkzz <sto134679@gmail.com>
2025-02-12 10:49:55 +0900

    FEAT / 스탯 enum용 스크립트 추가

---
+++ Arrow a Row clone/Assets/Scripts/StatType.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StatType
+{
+    HP,
+    MOVESPEED,
+
+    ARROWATK,
+    ARROWRATE,
+    ARROWSPEED,
+    ARROWRANGE,
+    ARROWCNT,
+
+    SWORDATK,
+    SWORDRATE,
+    SWORDSPEED,
+    SWORDRANGE,
+    SWORDCNT
+}
