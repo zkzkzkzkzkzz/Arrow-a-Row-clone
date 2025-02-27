e2291bbeaba8e19e62603427d3efc7f971305521
zkzkzkzkzkzz <sto134679@gmail.com>
2025-02-27 11:05:11 +0900

    FEAT / 상점 시스템 구현 및 JSON 파일 연동

--- Arrow a Row clone/Assets/Scripts/ShopManager.cs
+++ Arrow a Row clone/Assets/Scripts/ShopManager.cs
@@ -1,30 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using Newtonsoft.Json;
+using System.IO;
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine.SceneManagement;
 
 public class ShopManager : MonoBehaviour
 {
     public static ShopManager Instance { get; private set; }
 
-    // 기존 구매 정보
-    public int HPUpgrade;
-    public int ArrowATKUpgrade;
-    public bool OnBoard;
-
-    public int coins;
-
-    private string saveFilePath;
+    private string jsonPath;
+    private ShopData shopData;
+    private Player player;
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);  // 씬 전환 시에도 유지
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
@@ -32,53 +28,171 @@ public class ShopManager : MonoBehaviour
             return;
         }
 
-        saveFilePath = Application.persistentDataPath + "/shopData.dat";
-        LoadData();
+        jsonPath = Path.Combine(Application.persistentDataPath, "shopData.json");
+        LoadShopData();
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    public void SaveData()
+    private void LoadShopData()
     {
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(saveFilePath);
-
-        ShopData data = new ShopData();
-        data.HPUpgrade = HPUpgrade;
-        data.ArrowATKUpgrade = ArrowATKUpgrade;
-        data.OnBoard = OnBoard;
-        data.coins = coins;
-
-        bf.Serialize(file, data);
-        file.Close();
-    }
-
-    public void LoadData()
-    {
-        if (File.Exists(saveFilePath))
+        if (File.Exists(jsonPath))
         {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(saveFilePath, FileMode.Open);
-            ShopData data = (ShopData)bf.Deserialize(file);
-            file.Close();
-
-            HPUpgrade = data.HPUpgrade;
-            ArrowATKUpgrade = data.ArrowATKUpgrade;
-            OnBoard = data.OnBoard;
-            coins = data.coins;
+            string json = File.ReadAllText(jsonPath);
+            shopData = JsonConvert.DeserializeObject<ShopData>(json);
         }
         else
         {
-            // 초기값 설정
-            HPUpgrade = 0;
-            ArrowATKUpgrade = 0;
-            OnBoard = false;
-            coins = 100;  
+            shopData = new ShopData();
+            shopData.shop.items = new List<ShopItem>
+            {
+                new ShopItem
+                {
+                    id = "HP_Upgrade",
+                    name = "체력",
+                    effects = new List<int> { 100, 100, 100 },
+                    prices = new List<int> { 10, 10, 100 },
+                    purchasedLevels = new List<bool> { false, false, false }
+                },
+                new ShopItem
+                {
+                    id = "ArrowATK",
+                    name = "화살 공격력",
+                    effects = new List<int> { 1, 1, 1 },
+                    prices = new List<int> { 10, 10, 100 },
+                    purchasedLevels = new List<bool> { false, false, false }
+                }
+            };
+            SaveShopData();
         }
     }
 
-    public void AddCoins(int score)
+    private void SaveShopData()
     {
-        int coin = Mathf.RoundToInt(score * 0.026f);
-        coins += coin;
-        SaveData();
+        HashSet<string> existingIds = new HashSet<string>();
+        List<ShopItem> filteredItems = new List<ShopItem>();
+
+        foreach (var item in shopData.shop.items)
+        {
+            if (!existingIds.Contains(item.id))
+            {
+                existingIds.Add(item.id);
+                filteredItems.Add(item);
+            }
+        }
+
+        shopData.shop.items = filteredItems;
+
+        string json = JsonConvert.SerializeObject(shopData, Formatting.Indented);
+        File.WriteAllText(jsonPath, json);
     }
+
+    /// <summary>
+    /// 씬이 로드될 때 호출되는 함수
+    /// </summary>
+    /// <param name="scene">Scene 이름</param>
+    /// <param name="mode"></param>
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        player = FindObjectOfType<Player>();
+
+        if (player != null)
+            ApplyPurchasedItems();
+    }
+
+    public bool PurchaseItem(string itemId, int level)
+    {
+        ShopItem item = shopData.shop.items.Find(i => i.id == itemId);
+
+        if (item == null || level >= item.prices.Count || shopData.player.coins < item.prices[level])
+            return false;
+
+        if (item.purchasedLevels[level])
+            return false;
+
+        shopData.player.coins -= item.prices[level];
+        item.purchasedLevels[level] = true;
+        SaveShopData();
+
+        return true;
+    }
+
+    private void ApplyPurchasedItems()
+    {
+        if (player == null)
+            return;
+
+        foreach (var item in shopData.shop.items)
+        {
+            for (int i = 0; i < item.purchasedLevels.Count; ++i)
+                if (item.purchasedLevels[i])
+                    ApplyItemEffect(item.id, item.effects[i]);
+        }
+    }
+
+    private void ApplyItemEffect(string itemId, int value)
+    {
+        switch (itemId)
+        {
+            case "HP_Upgrade":
+                player.IncreaseStat(StatType.HP, value);
+                break;
+            case "ArrowATK":
+                player.IncreaseStat(StatType.ARROWATK, value);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public int GetPlayerCoins()
+    {
+        return shopData.player.coins;
+    }
+
+    public void SetPlayerCoins(int coin)
+    {
+        shopData.player.coins = coin;
+    }
+
+    public List<ShopItem> GetShopItems()
+    {
+        return shopData.shop.items;
+    }
+}
+
+
+[System.Serializable]
+public class ShopData
+{
+    public PlayerData player;
+    public Shop shop;
+
+    public ShopData()
+    {
+        player = new PlayerData();
+        shop = new Shop();
+    }
+}
+
+[System.Serializable]
+public class PlayerData
+{
+    public int coins = 100;
+}
+
+[System.Serializable]
+public class Shop
+{
+    public List<ShopItem> items;
+}
+
+[System.Serializable]
+public class ShopItem
+{
+    public string id;
+    public string name;
+    public List<int> effects;
+    public List<int> prices;
+    public List<bool> purchasedLevels;
 }
